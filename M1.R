8e4feeaf00af3969b4d88531b5f8cc45b134e470
dana-akesh <86303193+dana-akesh@users.noreply.github.com>
2023-07-07 15:19:49 +0300

    Calculate model (M1) accuracy using testing data
    - Implement accuracy calculation for the trained model
    - Evaluate the model's performance on the validation dataset

--- M1.R
+++ M1.R
@@ -1,29 +1,39 @@
 library(C50)
 
-# reading the file and putting the data in a dataset
-dataset <- read.csv("DiabetesData.csv", TRUE, ",")
+# reading the file and putting the data in a M1_dataset
+M1_dataset <- read.csv("DiabetesData.csv", TRUE, ",")
 
 # making the 0 & 1 values into
-dataset$Diabetic<-as.factor(dataset$Diabetic)
+M1_dataset$Diabetic<-as.factor(M1_dataset$Diabetic)
 
 set.seed(777)
 
 # ----
 # splitting the data and then train it
-# use 70% of dataset as training set and 30% as test set
+# use 70% of M1_dataset as training set and 30% as test set
 ctrl = C5.0Control(sample = 0.7)
-model = C5.0.default(dataset[ , -9], dataset[ , 9], control = ctrl)
+model = C5.0.default(M1_dataset[ , -9], M1_dataset[ , 9], control = ctrl)
 summary(model)
 
 
 # ----
 # plotting
 TrainingDataSet = ShuffledDataSet[1:floor(nrow(ShuffledDataSet)*0.7), ]
-ShuffledDataSet = dataset[order(runif(nrow(dataset))), ]
+ShuffledDataSet = M1_dataset[order(runif(nrow(M1_dataset))), ]
 M1_training_model <- C5.0.default(TrainingDataSet[, -9], TrainingDataSet[, 9])
 plot(M1_training_model, type="s", main=" M1 Decision Tree")
 
 # ----
+# calculate Accuracy
+# must predict data of 9th column
+result = predict(M2_training_model, TestingDataSet[ ,-9])
+tmp = result == TestingDataSet$Diabetic
+length(which(tmp)) # number of true
+length(tmp)# total number
+accuracy= length(which(tmp)) / length(tmp) * 100.0
 
+#print accuracy
+message <- sprintf("The Accuracy of the M2 model is %.2f", accuracy)
+print(message)
 
 
