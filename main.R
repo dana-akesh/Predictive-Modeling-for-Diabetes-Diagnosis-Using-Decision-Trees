b322de749d49b56372d3dbaa601673d3df4a5056
nadeenmoreb02 <132138078+nadeenmoreb02@users.noreply.github.com>
2023-07-07 17:42:59 +0300

    table

--- main.R
+++ main.R
@@ -49,3 +49,37 @@ statistics_table <- data.frame(
 
 #print(statistics_table)
 View(statistics_table)
+
+
+diabetic_column <- data$Diabetic
+
+zeros_n <- sum(diabetic_column == 0)
+ones_n <- sum(diabetic_column == 1)
+
+total <- length(diabetic_column)
+percentage_zeros <- (zeros_n / total) * 100
+percentage_ones <- (ones_n / total) * 100
+
+cat("Number of zeros:", num_zeros, "\n")
+cat("Number of ones:", num_ones, "\n")
+cat("Percentage of zeros:", percentage_zeros, "%\n")
+cat("Percentage of ones:", percentage_ones, "%\n")
+
+# Create a table for diabetics (1) and non-diabetics (0)
+diabetic_table <- table(diabetic_column)
+
+# Create a data frame from the table
+table_df <- as.data.frame(diabetic_table)
+
+# Add a column for percentages
+table_df$Percentage <- (table_df$Freq / sum(table_df$Freq)) * 100
+
+# Rename the columns
+colnames(table_df) <- c("Diabetic 1/ Non-Diabetic 0", "Count", "Percentage")
+
+#view table
+View(table_df)
+
+# Print the table
+print(table_df)
+
